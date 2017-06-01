1564e5078ab021c436e21c5c527fe65413242ae2
Juanse <juanseb@gmail.com>
2017-06-01 03:55:37 +0200

    to_s method, devise new attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password, :password_confirmation, :first_name, :last_name, :username, :organization_name)
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:email, :password, :password_confirmation, :current_password, :first_name, :last_name, :username, :organization_name)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def to_s
+     "#{username}"
+   end
 end
