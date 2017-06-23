5860fe8a8866cffad50fb37cc63db64249515e21
Juanse <juanseb@gmail.com>
2017-06-23 17:42:22 +0200

    Pundit implemented

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,22 @@
 class EventsController < ApplicationController
 before_action :set_event, only: [:show, :edit, :update, :destroy]
 before_action :authenticate_user!, except: [:index, :show]
-# before_action :authorize_owner!, only: [:edit, :update, :destroy]
+before_action :authorize_owner!, only: [:edit, :update, :destroy]
 
 	def show
-		#authorize @event, :show?
+		authorize @event, :show?
 	end
 	def index
 		@events = Event.order(created_at: :desc)
-		#authorize @events, :index?
+		authorize @events, :index?
 	end
 	def new
 		@event = Event.new
-		#authorize @event, :new?
+		authorize @event, :new?
 	end
 	def create
 		@event = Event.new(event_params)
-		#authorize @event, :create?
+		authorize @event, :create?
 		@event.organizer = current_user
 
 
@@ -30,12 +30,12 @@ before_action :authenticate_user!, except: [:index, :show]
 	end
 
 	def edit
-		#authorize @event, :edit?
+		authorize @event, :edit?
 	end
 	def update
 
 		if @event.update(event_params)
-			#authorize @event, :update?
+			authorize @event, :update?
 			flash[:notice] = "Updated!"
 			redirect_to @event
 		else
@@ -45,7 +45,7 @@ before_action :authenticate_user!, except: [:index, :show]
 
 	end
 	def destroy
-		#authorize @event, :destroy?
+		authorize @event, :destroy?
 		@event.destroy
 		flash.alert = " Deleted"
 		redirect_to events_url
@@ -65,14 +65,14 @@ before_action :authenticate_user!, except: [:index, :show]
 		flash.alert = "The page does not exist"
 		redirect_to events_path
 	end
-	# def authorize_owner!
-	# 	authenticate_user!
-	#
-	# 	unless @event.organizer == current_user
-	# 		flash[:alert] = "You do not have enough permission to '#{action_name}', the '#{@event.title.upcase}' event"
-	# 		redirect_to events_path
-	#
-	# 	end
-	# end
+	def authorize_owner!
+		authenticate_user!
+
+		unless @event.organizer == current_user
+			flash[:alert] = "You do not have enough permission to '#{action_name}', the '#{@event.title.upcase}' event"
+			redirect_to events_path
+
+		end
+	end
 
 end
