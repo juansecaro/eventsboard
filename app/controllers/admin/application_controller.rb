cb8d8ee1c4f3c57347e04e32e989a005636a1f7d
Juanse <juanseb@gmail.com>
2017-06-25 19:10:24 +0200

    Ability to create a new category by admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,13 +2,16 @@ class Admin::ApplicationController < ApplicationController
   before_action :authorize_admin!
   skip_after_action :verify_authorized
   def index
+    @users = User.all
     @events = Event.all
+    @categories = Category.all
+
   end
 
   private
   def authorize_admin!
     authenticate_user!
-    
+
     unless current_user.admin?
       redirect_to root_path, alert: "You must be admin"
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624071543) do
+ActiveRecord::Schema.define(version: 20170625134500) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "summary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
